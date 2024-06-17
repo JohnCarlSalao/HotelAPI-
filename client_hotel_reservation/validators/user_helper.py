83ab0125c43af61a4212ba8c748b2e68137eef3e
JohnCarls <salaojohn4@gmail.com>
2024-06-17 15:14:06 +0800

    Remove Secrets

--- client_hotel_reservation/validators/user_helper.py
+++ client_hotel_reservation/validators/user_helper.py
@@ -29,7 +29,5 @@ class UserHelper:
         if request.user.is_anonymous:
             errors['user'] = 'User is not logged in'
             return errors
-        if request.user.api_secret_key != request.META.get('HTTP_API_SECRET_KEY'):
-            errors['api_secret'] = 'Invalid API secret key'
-            return errors
+        
             
